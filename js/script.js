904241c5c93fff12ed2650529207d481a2d637e8
A1exLiu <21982749+A1exInamin@users.noreply.github.com>
2019-12-14 22:19:19 +0900

    Update css & js
    Signed-off-by: A1exLiu <21982749+A1exInamin@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -1,57 +1,80 @@
 $(function() {
-	var flag=false;
-	if(getQueryString("slide")==1){
-		flag=true;
+  var flag = false;
+  if (getQueryString("slide") == 1) {
+    flag = true;
 	}
-	// Slideshow 4
-	$("#slider4").responsiveSlides({
-	  auto: flag,
-	  pager: false,
-	  nav: false,
-	  speed: 500,
-	  namespace: "callbacks",
-	  before: function() {
-		$(".events").append("<li>before event fired.</li>");
-	  },
-	  after: function() {
-		$(".events").append("<li>after event fired.</li>");
-	  }
+
+  // Slideshow 4
+  $("#slider4").responsiveSlides({
+    auto: flag,
+    pager: false,
+    nav: false,
+    speed: 500,
+    namespace: "callbacks",
+    before: function() {
+      $(".events").append("<li>before event fired.</li>");
+    },
+    after: function() {
+      $(".events").append("<li>after event fired.</li>");
+    }
 	});
-  });
-$(document).ready(function(){
-
-	var times=getQueryString("times");
-	times=times.replace("!"," ");
-
-	var name=getQueryString("staff");
-	name=decodeURIComponent(name);
-	$('#staff').html(name);
-	/* ---- Countdown timer ---- */
-
-	$('#counter').countdown({
-		timestamp : (new Date(times)).getTime()
-	});
-
-
-	/* ---- Animations ---- */
-
-	$('#links a').hover(
-		function(){ $(this).animate({ left: 3 }, 'fast'); },
-		function(){ $(this).animate({ left: 0 }, 'fast'); }
-	);
-
-	$('footer a').hover(
-		function(){ $(this).animate({ top: 3 }, 'fast'); },
-		function(){ $(this).animate({ top: 0 }, 'fast'); }
-	);
-
-
-
 
 });
-var getQueryString = function (field, url) {
-	var href = url ? url : window.location.href;
-	var reg = new RegExp('[?&]' + field + '=([^&#]*)', 'i');
-	var string = reg.exec(href);
-	return string ? string[1] : null;
+
+$(document).ready(function() {
+  var times = getQueryString("times");
+  times = times.replace("!", " ");
+
+	var gpname = getQueryString("gpname");
+	switchEmoji(gpname);
+
+  var name = getQueryString("staff");
+  name = decodeURIComponent(name);
+  $("#staff").html(name);
+  /* ---- Countdown timer ---- */
+
+  $("#counter").countdown({
+    timestamp: new Date(times).getTime()
+  });
+
+  /* ---- Animations ---- */
+
+  $("#links a").hover(
+    function() {
+      $(this).animate({ left: 3 }, "fast");
+    },
+    function() {
+      $(this).animate({ left: 0 }, "fast");
+    }
+  );
+
+  $("footer a").hover(
+    function() {
+      $(this).animate({ top: 3 }, "fast");
+    },
+    function() {
+      $(this).animate({ top: 0 }, "fast");
+    }
+  );
+});
+var getQueryString = function(field, url) {
+  var href = url ? url : window.location.href;
+  var reg = new RegExp("[?&]" + field + "=([^&#]*)", "i");
+  var string = reg.exec(href);
+  return string ? string[1] : null;
 };
+
+function switchEmoji(type) {
+	if (type == "tamaki") {
+		$(".emoji").html("🐶");
+		$("#mirrorGroupName").html("犬金組");
+	}
+	else if (type == "hime") {
+		$(".emoji").html("🍡");
+		$("#mirrorGroupName").html("織田軍");
+	}
+	else if (type == "mishiro") {
+		$(".emoji").html("🐶❄️");
+		$("#mirrorGroupName").html("犬金組")
+	}
+}
