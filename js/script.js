8459d503f770ac7838c689f4bbbdf274d49b67aa
A1exLiu <21982749+A1exInamin@users.noreply.github.com>
2019-12-14 12:36:29 +0900

    Initial

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+$(document).ready(function(){
+
+
+	/* ---- Countdown timer ---- */
+
+	$('#counter').countdown({
+		timestamp : (new Date(2019,12,14)).getTime() + 20*60*60*1000
+	});
+
+
+	/* ---- Animations ---- */
+
+	$('#links a').hover(
+		function(){ $(this).animate({ left: 3 }, 'fast'); },
+		function(){ $(this).animate({ left: 0 }, 'fast'); }
+	);
+
+	$('footer a').hover(
+		function(){ $(this).animate({ top: 3 }, 'fast'); },
+		function(){ $(this).animate({ top: 0 }, 'fast'); }
+	);
+
+
+
+
+});
